65b42fd6f3b608baf651c10c823259e50addd604
Gabriele Palmeri <36270614+gab-palmeri@users.noreply.github.com>
2022-09-28 13:36:43 +0200

    Set-up project structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use druid::{AppLauncher, WindowDesc, Widget, PlatformError, WidgetExt, Color, Data, Lens};
+use druid::widget::{Flex, Container, TextBox};
+
+
+#[derive(Clone, Data, Lens)]
+struct HelloState {
+    name: String,
+    surname: String
+}
+
+fn build_ui() -> impl Widget<HelloState> {
+
+    Flex::column()
+        .with_flex_child(
+            TextBox::multiline()
+                .with_placeholder("Who are we greeting?")
+                .fix_height(400.0)
+                .fix_width(400.0)
+                .lens(HelloState::name),
+            50.0
+        )
+        .with_flex_child(
+            Container::new(
+                TextBox::multiline()
+                .with_placeholder("Who are we greeting?")
+                .fix_height(200.0)
+                .fix_width(200.0)
+                .lens(HelloState::surname).background(Color::RED),
+            ),
+            50.0
+        )
+}
+
+fn main() -> Result<(), PlatformError> {
+    
+    AppLauncher::with_window(WindowDesc::new(|| build_ui())).launch(HelloState { name: "test".into(), surname:"losbruglio".into() })?;
+    Ok(())
 }
