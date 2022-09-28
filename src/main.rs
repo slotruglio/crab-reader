a03573eedf9a165831f6089c02a5b27f3ffccf2e
SAMUELE LO TRUGLIO s295285 <s295285@studenti.polito.it>
2022-09-28 18:45:41 +0200

    epub + book widget

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,73 @@
-use druid::{AppLauncher, WindowDesc, Widget, PlatformError, WidgetExt, Color, Data, Lens};
-use druid::widget::{Flex, Container, TextBox};
-
+use druid::{AppLauncher, WindowDesc, Widget, PlatformError, WidgetExt, Color, Data, Lens, ImageBuf};
+use druid::widget::{Flex, Container, TextBox, Label, Image, FillStrat};
+use std::path::PathBuf;
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
+use epub::doc::EpubDoc;
 
 #[derive(Clone, Data, Lens)]
-struct HelloState {
-    name: String,
-    surname: String
+struct Book {
+    title: String,
+    author: String,
+    cover: String,
 }
 
-fn build_ui() -> impl Widget<HelloState> {
+impl Book {
+    fn new(path: &str) -> Book {
+        let mut book = EpubDoc::new(path).unwrap();
+        
+        let title = book.mdata("title").unwrap();
+        let author = book.mdata("creator").unwrap();
+        let cover_data = book.get_cover().unwrap();
 
-    Flex::column()
+        let mut title_to_save = title.clone();
+        title_to_save.push_str(".png");
+
+        println!("Saving cover to {}", title_to_save);
+
+        let copy_title = title_to_save.clone();
+        let f = File::create(copy_title);
+        assert!(f.is_ok());
+        let mut f = f.unwrap();
+        let resp = f.write_all(&cover_data);
+
+        Book{title, author, cover: title_to_save}
+    }
+}
+
+fn build_ui() -> impl Widget<Book> {
+
+    Flex::row()
         .with_flex_child(
-            TextBox::multiline()
-                .with_placeholder("Who are we greeting?")
-                .fix_height(400.0)
-                .fix_width(400.0)
-                .lens(HelloState::name),
-            50.0
+            Flex::column()
+                .with_child(
+                    Label::new(|data: &Book, _env: &_| data.title.clone())
+                    .with_text_color(Color::WHITE)
+                    .with_text_size(24.0)
+                    
+                )
+                .with_child(
+                    Label::new(|data: &Book, _env: &_| data.author.clone())
+                    .with_text_color(Color::GRAY)
+                    .with_text_size(20.0)
+                    
+                ),
+            4.0
         )
         .with_flex_child(
-            Container::new(
-                TextBox::multiline()
-                .with_placeholder("Who are we greeting?")
-                .fix_height(200.0)
-                .fix_width(200.0)
-                .lens(HelloState::surname).background(Color::RED),
-            ),
-            50.0
+            Label::new(|data: &Book, _env: &_| data.cover.clone())
+                    .with_text_color(Color::WHITE)
+                    .with_text_size(14.0)
+            ,
+            1.0
         )
 }
 
 fn main() -> Result<(), PlatformError> {
     
-    AppLauncher::with_window(WindowDesc::new(|| build_ui())).launch(HelloState { name: "test".into(), surname:"losbruglio".into() })?;
+    let book = Book::new("/Users/slotruglio/pds/crab-reader/src/assets/books/pg69058-images.epub");
+
+    AppLauncher::with_window(WindowDesc::new(|| build_ui())).launch(book)?;
     Ok(())
 }
