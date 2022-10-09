6e71c8d82a48c1b895c7babb1d95fe9e3d325cbe
SAMUELE LO TRUGLIO s295285 <s295285@studenti.polito.it>
2022-10-09 17:24:48 +0200

    add richtext support

--- src/utils/text_descriptor.rs
+++ src/utils/text_descriptor.rs
@@ -1,4 +1,4 @@
-use druid::{Widget, FontDescriptor, FontFamily, FontWeight, FontStyle};
+use druid::{Widget, FontDescriptor, FontFamily, FontWeight, FontStyle, ArcStr, text::{RichText, Attribute}};
 
 const H1: FontDescriptor = FontDescriptor::new(FontFamily::SYSTEM_UI)
     .with_size(24.0)
@@ -25,3 +25,23 @@ const ITALIC: FontDescriptor = FontDescriptor::new(FontFamily::SYSTEM_UI)
     .with_weight(FontWeight::NORMAL)
     .with_style(FontStyle::Italic);
 
+pub fn get_rich_text(text: String, tags: Vec<(usize, usize, String)>) -> RichText {
+    let buffer = ArcStr::from(text);
+    let mut rich_text = RichText::new(buffer);
+    for tag in tags {
+        let start = tag.0;
+        let end = tag.1;
+        let tag = tag.2;
+        
+        let attribute = match tag.as_str() {
+            "p" => P,
+            "strong" => BOLD,
+            "em" => ITALIC,
+            _ => P,
+        };
+        let val = Attribute::font_descriptor(attribute);
+        rich_text.add_attribute((start..end), val);
+
+    }
+    rich_text
+}
