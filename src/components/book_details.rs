586eadf65d70ce26148b8231958988a74cdd59b1
coccocarmiano <matteo.quarta.carmiano@gmail.com>
2022-10-15 19:07:25 +0200

    change: now uses a singel offset for all text, better margin handling

--- src/components/book_details.rs
+++ src/components/book_details.rs
@@ -5,13 +5,18 @@ use druid::{
 };
 use druid::{RenderContext, TextLayout};
 
-use super::{book::Book, library::Library};
+use super::{
+    book::GUIBook,
+    library::{GUILibrary, MockupLibrary},
+    mockup::MockupBook,
+};
 
 pub struct BookDetails {
-    title_offset_y: f64,
-    author_offset_y: f64,
-    description_offset_y: f64,
+    y_offset: f64,
 }
+
+type Book = MockupBook; // For compat
+type Library = MockupLibrary<Book>;
 
 impl Widget<Library> for BookDetails {
     fn event(&mut self, _: &mut EventCtx, _: &Event, _: &mut Library, _: &Env) {
@@ -33,9 +38,9 @@ impl Widget<Library> for BookDetails {
         data: &Library,
         _: &Env,
     ) -> druid::Size {
-        if data.get_selected_book().is_some() {
+        if let Some(_) = data.get_selected_book() {
             let w = bc.max().width;
-            let h = self.description_offset_y + 10.0;
+            let h = self.y_offset + 10.0;
             (w, h).into()
         } else {
             bc.min()
@@ -43,26 +48,20 @@ impl Widget<Library> for BookDetails {
     }
 
     fn paint(&mut self, ctx: &mut PaintCtx, data: &Library, env: &Env) {
-        if let Some(idx) = data.get_selected_book() {
-            if let Some(book) = data.get_book(idx) {
-                self.make_title(ctx, book, env);
-                self.make_author(ctx, book, env);
-                self.make_description(ctx, book, env)
-            }
+        if let Some(book) = data.get_selected_book() {
+            self.make_title(ctx, book, env);
+            self.make_author(ctx, book, env);
+            self.make_description(ctx, book, env)
         }
     }
 }
 
 impl BookDetails {
     pub fn new() -> Self {
-        Self {
-            title_offset_y: 0.,
-            author_offset_y: 0.,
-            description_offset_y: 0.,
-        }
+        Self { y_offset: 0.0 }
     }
 
-    fn make_title(&mut self, ctx: &mut PaintCtx, data: &Book, env: &Env) {
+    fn make_title(&mut self, ctx: &mut PaintCtx, data: &impl GUIBook, env: &Env) {
         let font_family = CairoText::new()
             .font_family("URW Bookman")
             .unwrap_or(FontFamily::SYSTEM_UI);
@@ -74,7 +73,7 @@ impl BookDetails {
         let lmargin = 7.5;
 
         let mut layout: TextLayout<String> = TextLayout::new();
-        layout.set_text(data.get_title());
+        layout.set_text(data.get_title().to_string());
         layout.set_text_color(Color::WHITE);
         layout.set_font(font);
         layout.set_wrap_width(ctx.size().width - 2. * lmargin - 10.0);
@@ -82,17 +81,17 @@ impl BookDetails {
 
         let pos = Point::new(
             (ctx.size().width / 2.0 - layout.size().width / 2.0).max(lmargin) - 5.0,
-            layout.size().height + 5.0,
+            10.0,
         );
 
-        self.title_offset_y = pos.y + layout.size().height;
+        self.y_offset = layout.size().height;
 
         if let Some(layout) = layout.layout() {
             ctx.draw_text(layout, pos);
         }
     }
 
-    fn make_author(&mut self, ctx: &mut PaintCtx, data: &Book, env: &Env) {
+    fn make_author(&mut self, ctx: &mut PaintCtx, data: &impl GUIBook, env: &Env) {
         let font_family = CairoText::new()
             .font_family("URW Bookman")
             .unwrap_or(FontFamily::SYSTEM_UI);
@@ -104,7 +103,7 @@ impl BookDetails {
         let lmargin = 7.5;
 
         let mut layout: TextLayout<String> = TextLayout::new();
-        layout.set_text(data.get_author());
+        layout.set_text(data.get_author().to_string());
         layout.set_text_color(Color::WHITE);
         layout.set_font(font);
         layout.set_wrap_width(ctx.size().width - 2. * lmargin - 10.0);
@@ -112,17 +111,17 @@ impl BookDetails {
 
         let pos = Point::new(
             (ctx.size().width / 2.0 - layout.size().width / 2.0).max(lmargin) - 5.0,
-            self.title_offset_y + 5.0,
+            self.y_offset + layout.size().height,
         );
 
-        self.author_offset_y = pos.y + layout.size().height;
+        self.y_offset = pos.y + layout.size().height;
 
         if let Some(layout) = layout.layout() {
             ctx.draw_text(layout, pos);
         }
     }
 
-    fn make_description(&mut self, ctx: &mut PaintCtx, data: &Book, env: &Env) {
+    fn make_description(&mut self, ctx: &mut PaintCtx, data: &impl GUIBook, env: &Env) {
         let font_family = CairoText::new()
             .font_family("URW Bookman")
             .unwrap_or(FontFamily::SYSTEM_UI);
@@ -135,7 +134,7 @@ impl BookDetails {
 
         let lmargin = 15.0;
 
-        layout.set_text(data.get_description());
+        layout.set_text(data.get_description().to_string());
         layout.set_text_color(Color::WHITE);
         layout.set_font(font);
         layout.set_wrap_width(ctx.size().width - 2. * lmargin - 5.0);
@@ -143,10 +142,10 @@ impl BookDetails {
 
         let pos = Point::new(
             (ctx.size().width / 2.0 - layout.size().width / 2.0).max(lmargin) - 5.0,
-            self.author_offset_y + 5.0,
+            self.y_offset + 20.0,
         );
 
-        self.description_offset_y = pos.y + layout.size().height;
+        self.y_offset = pos.y + layout.size().height;
 
         if let Some(layout) = layout.layout() {
             ctx.draw_text(layout, pos);
