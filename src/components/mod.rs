8244687ce77f8915ab5a50ca3004bebc4e6786ec
coccocarmiano <matteo.quarta.carmiano@gmail.com>
2022-10-15 19:05:04 +0200

    new: new mods for new component split

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,6 +1,10 @@
 pub mod book;
+pub mod book_cover;
 pub mod book_details;
+pub mod book_listing;
+pub mod cover_library;
+pub mod display_mode_button;
 pub mod header;
 pub mod library;
+pub mod listing_library;
 pub mod mockup;
-pub mod view_switcher;
