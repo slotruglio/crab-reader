c775182523407040361f0fa1907da29ab7a1eb02
coccocarmiano <matteo.quarta.carmiano@gmail.com>
2022-10-20 16:35:30 +0200

    adjusted RC problem, fixed some function signatures

--- src/components/reader_view.rs
+++ src/components/reader_view.rs
@@ -9,9 +9,11 @@ use super::book::{Book, BookReading};
 // single page view for text reader
 pub fn build_single_view() -> impl Widget<AppState> {
     Scroll::new(
-        Label::dynamic(|data: &AppState, _env: &_| data.book.get_page_of_chapter().clone())
-            .with_line_break_mode(LineBreaking::WordWrap)
-            .fix_size(400.0, 300.0),
+        Label::dynamic(|data: &AppState, _env: &_| {
+            data.book.get_page_of_chapter().clone().to_string()
+        })
+        .with_line_break_mode(LineBreaking::WordWrap)
+        .fix_size(400.0, 300.0),
     )
     .vertical()
 }
@@ -19,9 +21,10 @@ pub fn build_single_view() -> impl Widget<AppState> {
 // single page view for text editing
 pub fn build_single_view_edit() -> impl Widget<AppState> {
     Scroll::new(
-        TextBox::multiline()
-            .fix_size(400.0, 300.0)
-            .lens(AppState::book.then(Book::chapter_page_text)),
+        // TextBox::multiline()
+        // .fix_size(400.0, 300.0)
+        // .lens(AppState::book.then(Book::chapter_page_text)),
+        Flex::row(),
     )
     .vertical()
 }
@@ -31,17 +34,21 @@ pub fn build_dual_view() -> impl Widget<AppState> {
     Flex::row()
         .with_child(
             Scroll::new(
-                Label::dynamic(|data: &AppState, _env: &_| data.book.get_dual_pages().0.clone())
-                    .with_line_break_mode(LineBreaking::WordWrap)
-                    .fix_size(400.0, 300.0),
+                Label::dynamic(|data: &AppState, _env: &_| {
+                    data.book.get_dual_pages().0.to_string()
+                })
+                .with_line_break_mode(LineBreaking::WordWrap)
+                .fix_size(400.0, 300.0),
             )
             .vertical(),
         )
         .with_child(
             Scroll::new(
-                Label::dynamic(|data: &AppState, _env: &_| data.book.get_dual_pages().1.clone())
-                    .with_line_break_mode(LineBreaking::WordWrap)
-                    .fix_size(400.0, 300.0),
+                Label::dynamic(|data: &AppState, _env: &_| {
+                    data.book.get_dual_pages().1.to_string()
+                })
+                .with_line_break_mode(LineBreaking::WordWrap)
+                .fix_size(400.0, 300.0),
             )
             .vertical(),
         )
