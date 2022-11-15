13e8ca334cbecaf206e29f8666709c7c5712f877
coccocarmiano <matteo.quarta.carmiano@gmail.com>
2022-11-15 17:19:09 +0100

    fixed colors

--- src/components/colors.rs
+++ src/components/colors.rs
@@ -4,6 +4,6 @@ pub const ACCENT_GRAY: Color = Color::rgb8(100, 100, 100);
 pub const NORMAL_GRAY: Color = Color::rgb8(60, 60, 60);
 pub const HOT_GRAY: Color = Color::rgb8(80, 80, 80);
 pub const ACTIVE_GRAY: Color = Color::rgb8(100, 100, 100);
-pub const BG_GRAY: Color = Color::grey8(64);
+pub const BG_GRAY: Color = Color::grey8(32);
 pub const TEXT_WHITE: Color = Color::rgb8(255, 255, 255);
 pub const TEXT_BLACK: Color = Color::rgb8(0, 0, 0);
