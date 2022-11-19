52eaf5bd1d5de0f631fc50977e4c33a748933f3b
coccocarmiano <matteo.quarta.carmiano@gmail.com>
2022-11-19 17:15:46 +0100

    added fav indicator to listing

--- src/components/book_listing.rs
+++ src/components/book_listing.rs
@@ -12,6 +12,7 @@ pub struct BookListing<T> {
     is_hot: bool,
     title_label: WidgetPod<T, Label<T>>,
     page_cnt_label: WidgetPod<T, Label<T>>,
+    star: WidgetPod<T, Label<T>>,
 }
 
 impl<T: GUIBook> BookListing<T> {
@@ -32,10 +33,15 @@ impl<T: GUIBook> BookListing<T> {
         .with_text_color(colors::TEXT_WHITE)
         .with_line_break_mode(LineBreaking::WordWrap);
 
+        let star_label =
+            Label::dynamic(|data: &T, _| if data.is_favorite() { "🌟" } else { "" }.into())
+                .with_font(fonts::Font::default().md().emoji().get());
+
         Self {
             is_hot: false,
             title_label: WidgetPod::new(title_label),
             page_cnt_label: WidgetPod::new(page_cnt_label),
+            star: WidgetPod::new(star_label),
         }
     }
 
@@ -51,9 +57,10 @@ impl<T: GUIBook> BookListing<T> {
 }
 
 impl<B: GUIBook + Data> Widget<B> for BookListing<B> {
-    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut B, _: &Env) {
-        self.title_label.event(ctx, event, data, &Env::default());
-        self.page_cnt_label.event(ctx, event, data, &Env::default());
+    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut B, env: &Env) {
+        self.title_label.event(ctx, event, data, env);
+        self.page_cnt_label.event(ctx, event, data, env);
+        self.star.event(ctx, event, data, env);
 
         match event {
             Event::MouseDown(_) => {
@@ -78,12 +85,14 @@ impl<B: GUIBook + Data> Widget<B> for BookListing<B> {
     fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &B, env: &Env) {
         self.title_label.lifecycle(ctx, event, data, env);
         self.page_cnt_label.lifecycle(ctx, event, data, env);
+        self.star.lifecycle(ctx, event, data, env);
     }
 
     fn update(&mut self, ctx: &mut UpdateCtx, old_data: &B, data: &B, env: &Env) {
         if !old_data.same(data) {
             self.title_label.update(ctx, data, env);
             self.page_cnt_label.update(ctx, data, env);
+            self.star.update(ctx, data, env);
         }
     }
 
@@ -98,18 +107,28 @@ impl<B: GUIBook + Data> Widget<B> for BookListing<B> {
         };
 
         let title_w = w * 0.65;
+        let star_w = w * 0.1;
         let page_cnt_w = w * 0.25;
 
         let title_bc = BoxConstraints::new((title_w, minh).into(), (title_w, maxh).into());
         let page_cnt_bc = BoxConstraints::new((minh, minh).into(), (page_cnt_w, maxh).into());
+        let star_bc = BoxConstraints::new((star_w, minh).into(), (star_w, maxh).into());
 
         let title_label_size = self.title_label.layout(ctx, &title_bc, data, env);
         let page_cnt_label_size = self.page_cnt_label.layout(ctx, &page_cnt_bc, data, env);
+        let star_size = self.star.layout(ctx, &star_bc, data, env);
 
         let h = title_label_size.height.max(page_cnt_label_size.height) + 20.0;
 
         let title_origin = (10.0, h / 2. - title_label_size.height / 2.).into();
         self.title_label.set_origin(ctx, data, env, title_origin);
+
+        let star_origin = (
+            title_origin.x + title_label_size.width + 10.0,
+            h / 2. - star_size.height / 2.,
+        )
+            .into();
+        self.star.set_origin(ctx, data, env, star_origin);
 
         let page_cnt_origin = (
             w - page_cnt_label_size.width - 10.,
@@ -129,5 +148,6 @@ impl<B: GUIBook + Data> Widget<B> for BookListing<B> {
 
         self.title_label.paint(ctx, data, env);
         self.page_cnt_label.paint(ctx, data, env);
+        self.star.paint(ctx, data, env);
     }
 }
